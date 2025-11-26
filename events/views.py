55dd30297a1733290d616f3de1558ff038659337
Maiko <ewigemagie@outlook.com>
2025-11-26 16:46:12 -0300

    feat: add inscrever_evento view

--- events/views.py
+++ events/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect, get_object_or_404
+from .models import Evento
+from .forms import InscricaoEventoForm
 
 def index(request):
     return render(request, 'index.html')
+
+def inscrever_evento(request, evento_id):
+    evento = get_object_or_404(Evento, pk=evento_id)
+    
+    if request.method == 'POST':
+        form = InscricaoEventoForm(request.POST)
+        if form.is_valid():
+            inscricao = form.save(commit=False)
+            inscricao.participante = request.user
+            inscricao.evento = evento
+            inscricao.save()
+            return redirect('detalhes_evento', pk=evento.id)
+    else:
+        form = InscricaoEventoForm()
+
+    return render(request, 'inscricao.html', {'form': form, 'evento': evento})
