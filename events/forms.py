f6f80a0beee7626658af4c9d44dcea00f4157bfe
Maiko <ewigemagie@outlook.com>
2025-11-26 16:44:26 -0300

    feat: add UsuarioForm to forms.py

--- events/forms.py
+++ events/forms.py
@@ -10,3 +10,13 @@ class InscricaoEventoForm(forms.ModelForm):
     def clean(self):
         # Validação extra: verificar se já existe inscrição pode ser feito aqui ou na View
         pass
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['nome', 'idade', 'tipo']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'idade': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-select'}),
+        }
