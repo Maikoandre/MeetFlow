4674e192715a4c6ae511e4266e408369c09840ff
Maiko <ewigemagie@outlook.com>
2025-11-30 16:01:38 -0300

    feat: add new urls

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # --- Home e Públicas ---
     path('', views.index, name='index'),
+    path('cadastro/', views.cadastro_usuario, name='cadastro_usuario'),
+    path('evento/<int:evento_id>/inscrever/', views.inscrever_evento, name='inscrever_evento'),
+    # path('evento/<int:pk>/', views.detalhes_evento, name='detalhes_evento'),
+    path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
+    path('perfil/senha/', views.alterar_senha, name='alterar_senha'),
 ]
